1d742bf1bcc9c74906e37eaafa92e2d7cd039cd0
KhatriSuson <susonkhatri110@gmail.com>
2024-08-31 17:27:26 +0545

    initial commit

--- FORENSICUK/forensicApp/urls.py
+++ FORENSICUK/forensicApp/urls.py
@@ -4,4 +4,9 @@ from forensicApp import views
 
 urlpatterns = [
     path('', views.home, name="home" ),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name="contact"),
+    path('test/', views.test, name='test'),
 ]
