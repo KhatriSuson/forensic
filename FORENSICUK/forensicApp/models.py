263b311c3b160e45027df5ecfe24003119ac1337
KhatriSuson <susonkhatri110@gmail.com>
2024-09-08 09:40:11 +0545

    initial commit

--- FORENSICUK/forensicApp/models.py
+++ FORENSICUK/forensicApp/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# class Slide(models.Model):
+#     title = models.CharField(max_length=200)
+#     descripitoin = models.TextField(blank=True)
+#     image = models.ImageField(upload_to='media/slide')
+#     order = models.IntegerField(default=0)    
+
+#     def __str__(self):
+#         return self.title
+    
 class Home(models.Model):
     image = models.ImageField(upload_to='media/home')
     title = models.CharField(max_length=100)
