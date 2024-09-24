9a2a183d0c8245909ee2b79facf7aea77817a28b
KhatriSuson <susonkhatri110@gmail.com>
2024-09-24 22:50:28 +0545

    urls blog_list

--- FORENSICUK/forensicApp/urls.py
+++ FORENSICUK/forensicApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name="home" ),
     path('about/', views.about, name='about'),
     path('service/', views.service, name='service'),
-    path('blog/', views.blog, name='blog'),
+    path('blogs/', views.blog_list, name='blog_list'),
     path('contact/', views.contact, name="contact"),
     path('test/', views.test, name='test'),
 ]
