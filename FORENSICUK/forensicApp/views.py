2a43586cd402a64f6e42e0a007cfe3e24f1a1c26
KhatriSuson <susonkhatri110@gmail.com>
2024-09-26 22:22:29 +0545

    initial commit

--- FORENSICUK/forensicApp/views.py
+++ FORENSICUK/forensicApp/views.py
@@ -21,6 +21,7 @@ def home(request):
     
     # Add the paginated abouts to the context
     views['page_obj'] = page_obj
+   
 
     # Handle POST request for contact form submission
     if request.method == "POST":
@@ -60,12 +61,28 @@ def about_detail(reqeust, pk):
     about = get_object_or_404(About, pk=pk)
     return render(reqeust, 'about_detail.html', {'about':about})
 
-def service(request):
-    views = {}
-    views['services'] = Service.objects.all()
-    views['members'] = Member.objects.all()
-    return render(request, 'service.html', views)
+# def service(request):
+#     views = {}
+#     views['members'] = Member.objects.all()
+    
+#     services = Service.objects.all()
+#     paginator = Paginator(services,3)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     views['page_obj'] = page_obj
+#     return render(request, 'service.html', views)
 
+
+
+def services_view(request):
+    # Assuming `services` is a QuerySet of service objects
+    services = Service.objects.all()
+
+    # Set up pagination
+    paginator = Paginator(services, 2)  # Show 6 services per page
+    page_number = request.GET.get('page')  # Get the page number from the URL
+    page_obj = paginator.get_page(page_number)  # Get the relevant page
+    return render(request, 'service.html', {'page_obj': page_obj})
 
 
 def contact(request):
