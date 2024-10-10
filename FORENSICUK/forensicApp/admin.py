275884877e6c6e6559b96454ed86cf94ba8c7229
KhatriSuson <susonkhatri110@gmail.com>
2024-10-10 19:50:05 +0545

    blogpost update

--- FORENSICUK/forensicApp/admin.py
+++ FORENSICUK/forensicApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About, Service, Member, Feedback, Contact, CarouselItem, Blog
+from .models import About, Service, Member, Feedback, Contact, CarouselItem, Blog, BlogPost
 # Register your models here.
 admin.site.register(About)
 admin.site.register(Service)
@@ -7,6 +7,8 @@ admin.site.register(Member)
 admin.site.register(Feedback)
 admin.site.register(Contact)
 admin.site.register(CarouselItem)
+
+admin.site.register(BlogPost)
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
