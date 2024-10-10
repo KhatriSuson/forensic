56bb8cf6020e6f5d5d158ee8f83a59072f7ca7a1
KhatriSuson <susonkhatri110@gmail.com>
2024-10-10 19:47:18 +0545

    initial commit

--- FORENSICUK/forensicApp/models.py
+++ FORENSICUK/forensicApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
 
 class About(models.Model):
     title = models.CharField(max_length=100)
@@ -74,3 +75,35 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+    
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def total_likes(self):
+        return self.likes.count()
+    
+    def total_comments(self):
+        return self.comments.count()
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='likes')
+    
+    class Meta:
+        unique_together = ('user', 'post')
+        
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.post.title}'
+    
