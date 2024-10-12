b0a26628ef55cf5770a06c5cfd28d189e234a9b7
KhatriSuson <susonkhatri110@gmail.com>
2024-10-12 08:56:01 +0545

    thank_you.html

--- FORENSICUK/FORENSICUK/settings.py
+++ FORENSICUK/FORENSICUK/settings.py
@@ -138,3 +138,12 @@ CKEDITOR_CONFIGS = {
         'width': '100%',
     },
 }
+
+
+# Subscribe Email Configuration 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your-email-password'
