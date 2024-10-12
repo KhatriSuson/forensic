877b889c310baca6ed950e3dc3a1da833f0cb41f
KhatriSuson <susonkhatri110@gmail.com>
2024-10-12 09:00:48 +0545

    subscription and newsletter

--- FORENSICUK/forensicApp/admin.py
+++ FORENSICUK/forensicApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import About, Service, Member, Feedback, Contact, CarouselItem, Blog
+from .models import About, Service, Member, Feedback, Contact, CarouselItem, Blog, Subscriber, Newsletter
+from django.core.mail import send_mail
 # Register your models here.
 admin.site.register(About)
 admin.site.register(Service)
@@ -14,3 +15,19 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     search_fields = ('title',)
     list_filter = ('created_at',)
+
+@admin.action(description='Send Newsletter')
+def send_newsletter(modeladmin, request, queryset):
+    subscribers = Subscriber.objects.filter(subscribed=True)
+    for newsletter in queryset:
+        subject = newsletter.subject
+        message = newsletter.message
+        recipient_list = [subscriber.email for subscriber in subscribers]
+        send_mail(subject, message, 'your-email@gmail.com', recipient_list)
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'created_at']
+    actions = [send_newsletter]
+
+admin.site.register(Subscriber)
+admin.site.register(Newsletter, NewsletterAdmin)
